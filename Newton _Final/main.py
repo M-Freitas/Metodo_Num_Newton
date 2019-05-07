d5b1dbee08d0b806e6e62629fb84cdd6d96b60cc
Marcos Freitas <marcoszfreitasz@outlook.com>
2019-05-07 09:07:45 -0300

    Update main.py

--- Newton _Final/main.py
+++ Newton _Final/main.py
@@ -3,7 +3,7 @@ import funcs, menu
 from prettytable import PrettyTable
 ######
 menu.menu()
-print("-------- Atribuição de Valores --------")
+print("-------- Atribuição de Valores --------\n")
 a_3 = funcs.dif_zero('A_(3)', 'float')
 a_2 = funcs.dif_zero('A_(2)', 'float')
 d_0 = funcs.dif_zero('D_(0) - [Aproximação Inicial]', 'float')
@@ -27,7 +27,7 @@ table_values.add_row(["Iterações Máximas: %s" %iterMax])
 print(table_values)
 opc_met = int(input("Escolha um das opções para selecionar o método a ser utilizado - [1 - MÉTODO DE NEWTON || 2 - MÉTODO DE NEWTON(FL)]\nOpcão: "))
 while (opc_met != 1 and opc_met != 2):
-	opc_met = int(input("Escolha um das opções listas - [1 - Método de Newton || 2 - Método de Newton(FL)]\nOpção: "))
+	opc_met = int(input("Escolha um das opções listas\n[1 - Método de Newton || 2 - Método de Newton(FL)]\nOpção: "))
 menu.enter('met')
 menu.clear()
 if opc_met == 1:
